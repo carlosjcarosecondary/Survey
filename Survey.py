441bebd63d103efb4cabd64a2ebffccf8b7128ff
Carlos Jesus Caro <carlos.jesus-caro@edu.dsti.institute>
2020-04-30 09:55:55 -0400

    Getting all Survey IDs

--- Survey.py
+++ Survey.py
@@ -7,9 +7,17 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 # Reference
-cursor = conn.cursor()
-cursor.execute('SELECT U.UserId, U.[User_Name], COALESCE ((SELECT A.Answer_Value FROM Answer as A WHERE A.SurveyId = 1 AND A.QuestionId = 1 AND A.UserId = U.UserId), -1) as Q1 FROM [User] as U')
-for row in cursor:
-    print(row)
+#cursor = conn.cursor()
+#cursor.execute('SELECT U.UserId, U.[User_Name], COALESCE ((SELECT A.Answer_Value FROM Answer as A WHERE A.SurveyId = 1 AND A.QuestionId = 1 AND A.UserId = U.UserId), -1) as Q1 FROM [User] as U')
+#for row in cursor:
+#    print(row)
 
-#Testing the connection 
+# 2. Getting all Survey IDs
+cursor = conn.cursor()
+cursor.execute('SELECT SurveyId FROM Survey ORDER BY SurveyId')
+surveyIDs = []
+for row in cursor:
+    element = [elem for elem in row]
+    surveyIDs.append(element[0])
+
+
